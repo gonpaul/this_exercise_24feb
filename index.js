ff764cda13f01c082b615ef8b45579cf456e79a2
gonpaul <pavelgoncharov56@gmail.com>
2024-02-12 10:15:01 +0300

    this_exercise

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 // @ts-check
+// import half from './src/half.js';
+// export default half;
 
-import half from './src/half.js';
+import readlineSync from 'readline-sync';
 
-export default half;
+const calculator = {
+  a: 0,
+  b: 0,
+
+  read() {
+    this.a = +readlineSync.question('Введите значение для a: ');
+    this.b = +readlineSync.question('Введите значение для b: ');
+  },
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+};
+
+calculator.read();
+console.log(`Сумма: ${calculator.sum()}`);
+console.log(`Произведение: ${calculator.mul()}`);
